6e2512834343fb8f1f3531bb47b9bba4f813592f
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-03-09 09:58:43 -0300

    criação da logica para funcionamento da calculadora

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.button.MaterialButton;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -55,6 +57,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         MaterialButton button = (MaterialButton) view;
         String buttonText = button.getText().toString();
-        solutionTv.setText(buttonText);
+        String dataToCalculate = solutionTv.getText().toString();
+
+        // Lógica para limpar a tela com o botão AC
+
+        if (buttonText.equals("AC")) {
+            solutionTv.setText("");
+            resultTv.setText("0");
+            return;
+        }
+
+        // Lógica para mostrar o resultado na tela de solution.
+
+        if (buttonText.equals("=")) {
+            solutionTv.setText(resultTv.getText());
+            return;
+        }
+
+        if (buttonText.equals("C")) {
+            dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length()-1);
+        } else {
+            dataToCalculate += buttonText;
+        }
+
+        solutionTv.setText(dataToCalculate);
+
+        String finalResult = getResult(dataToCalculate);
+
+        if (!finalResult.equals("Err")) {
+            resultTv.setText(finalResult);
+        }
+    }
+
+    String getResult(String data) {
+
+        try {
+            Context context = Context.enter();
+            context.setOptimizationLevel(-1);
+
+            Scriptable scriptable = context.initStandardObjects();
+            String finalResult =  context.evaluateString(scriptable, data, "Javascript", 1, null).toString();
+
+            if (finalResult.endsWith(".0")) {
+                finalResult = finalResult.replace(".0", "");
+            }
+
+            return finalResult;
+        } catch (Exception e) {
+            return "Err";
+        }
     }
 }
